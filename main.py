789dbc222600b567719f8fb12836c1a6c1cb7dc8
vietanhlee <levietanhtrump@gmail.com>
2025-01-29 02:32:16 +0700

    update qt5

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import cv2
 from OcrPlate import OcrPlate
 
-path_plate = 'best_plate.pt'
-path_ocr = 'best_ocr.pt'
+path_plate = 'model/best_plate.pt'
+path_ocr = 'model/best_ocr.pt'
+
 cam = cv2.VideoCapture(r"test.MOV")
 
 if not cam.isOpened():
@@ -17,14 +18,19 @@ while True:
     if(check == False):
         print('Không đọc được ảnh')
         break   
-
+    # Chuyển về ảnh RGB vì model được huấn luyện đọc ảnh bằng rgb
     cap_rgb = cv2.cvtColor(cap, cv2.COLOR_BGR2RGB)
 
     Dectect = OcrPlate(path_model_detect_plate= path_plate, path_model_ocr= path_ocr, imgage_input= cap_rgb)
+    # Lấy ra img cuối cùng sau khi đã vẽ các box và đánh nhãn các biển số
     img_out = Dectect.image_output
+    # Chuyển về BRG để cv2 hiển thị lên màn 
     img_out = cv2.cvtColor(img_out, cv2.COLOR_BGR2RGB)
-
-    cv2.imshow('app', img_out)
-
+    # Ghi ra màn
+    cv2.imshow('test code', img_out)
+    
     if cv2.waitKey(1) & 0XFF == ord('q'):
         break
+
+cam.release()
+cv2.destroyAllWindows()
